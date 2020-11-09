d133b05b4f00f9e11da8f219f7bd2ca80ec93fb3
emirakbay <akbayemir@gmail.com>
2020-11-09 22:04:11 +0300

    Add AnimationStateController.cs
    AnimationStateController.cs to set animation controller states. Breakdance1990.anim is in use as box breaking animation.

---
+++ RunDrill/Assets/Scripts/AnimationStateController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class AnimationStateController : MonoBehaviour
+{
+    Animator animator;
+    int isBreakingHash;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        isBreakingHash = Animator.StringToHash("isBreaking");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool mouseButton = Input.GetMouseButton(0);
+
+        bool isBreaking = animator.GetBool(isBreakingHash);
+
+        if (mouseButton && !isBreaking)
+        {
+            animator.SetBool(isBreakingHash, true);
+        }
+
+        if (!mouseButton && isBreaking)
+        {
+            animator.SetBool(isBreakingHash, false);
+        }
+    }
+}
